5d0b52114855f02a843eb03345ed0aab767f4ed8
Rodrigo <631210356@SALA704-18.fspoaeduc.com.br>
2013-04-17 09:03:27 -0300

    Ajustado o layout com o grup, adicionado o Thema blueLight e adicionado imagem.

--- Momoca/src/TelaVenda.java
+++ Momoca/src/TelaVenda.java
@@ -1,14 +1,12 @@
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -19,16 +17,9 @@ import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
-import javax.swing.table.DefaultTableModel;
-import java.awt.Color;
 import javax.swing.border.TitledBorder;
-import javax.swing.border.BevelBorder;
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.EtchedBorder;
-import javax.swing.border.LineBorder;
-import javax.swing.border.MatteBorder;
-import javax.swing.border.SoftBevelBorder;
-
+import javax.swing.table.DefaultTableModel;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class TelaVenda extends JFrame {
 
@@ -36,9 +27,9 @@ public class TelaVenda extends JFrame {
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
-	private JDiag jDiag;
 	private JTextField textField_3;
 	private JTable table;
+	private JLabel lblFoto;
 
 	/**
 	 * Launch the application.
@@ -61,87 +52,65 @@ public class TelaVenda extends JFrame {
 	 */
 	public TelaVenda() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 889, 634);
+		setBounds(100, 100, 976, 636);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(153, 204, 255));
+		contentPane.setBackground(UIManager.getColor("CheckBox.light"));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
 
 		JLabel lblTotal = new JLabel("Total R$");
 		lblTotal.setFont(new Font("Verdana", Font.PLAIN, 35));
-		lblTotal.setBounds(10, 528, 161, 58);
-		contentPane.add(lblTotal);
 
 		textField = new JTextField();
 		textField.setHorizontalAlignment(SwingConstants.RIGHT);
 		textField.setEditable(false);
 		textField.setFont(new Font("Verdana", Font.PLAIN, 35));
 		textField.setText("135,54");
-		textField.setBounds(181, 528, 197, 58);
-		contentPane.add(textField);
 		textField.setColumns(10);
-		
-		
+
 		JLabel lblProduto = new JLabel("Valor Unit\u00E1rio R$");
 		lblProduto.setFont(new Font("Verdana", Font.PLAIN, 20));
-		lblProduto.setBounds(480, 408, 180, 31);
-		contentPane.add(lblProduto);
-		
+
 		JTextPane txtpnFarinhaDeManioca = new JTextPane();
 		txtpnFarinhaDeManioca.setEditable(false);
 		txtpnFarinhaDeManioca.setText("FARINHA DE MANDIOCA - GRANFINO - 500GR");
 		txtpnFarinhaDeManioca.setFont(new Font("Verdana", Font.PLAIN, 20));
-		txtpnFarinhaDeManioca.setBounds(480, 323, 363, 74);
-		contentPane.add(txtpnFarinhaDeManioca);
-		
+
 		JLabel lblPreoUnitrio = new JLabel("Qnt");
 		lblPreoUnitrio.setHorizontalAlignment(SwingConstants.LEFT);
 		lblPreoUnitrio.setFont(new Font("Verdana", Font.PLAIN, 20));
-		lblPreoUnitrio.setBounds(766, 246, 77, 31);
-		contentPane.add(lblPreoUnitrio);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setEditable(false);
 		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		textField_1.setText("12345643434");
+		textField_1.setText("0123456789123");
 		textField_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		textField_1.setBounds(480, 282, 249, 30);
-		contentPane.add(textField_1);
 		textField_1.setColumns(10);
-		
+
 		JLabel lblQuantidade = new JLabel("C\u00F3digo de Barras");
 		lblQuantidade.setFont(new Font("Verdana", Font.PLAIN, 20));
-		lblQuantidade.setBounds(480, 246, 233, 31);
-		contentPane.add(lblQuantidade);
-		
+
 		textField_2 = new JTextField();
 		textField_2.setEditable(false);
 		textField_2.setText("5");
 		textField_2.setHorizontalAlignment(SwingConstants.RIGHT);
 		textField_2.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		textField_2.setColumns(10);
-		textField_2.setBounds(766, 282, 77, 30);
-		contentPane.add(textField_2);
+
+		lblFoto = new JLabel("");
+		lblFoto.setIcon(new ImageIcon("Imagens/pao.jpg"));
+		lblFoto.setOpaque(true);
 		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setOpaque(true);
-		lblNewLabel.setBounds(480, 25, 363, 210);
-		contentPane.add(lblNewLabel);
-		
+
 		textField_3 = new JTextField();
 		textField_3.setEditable(false);
 		textField_3.setFont(new Font("Verdana", Font.PLAIN, 20));
 		textField_3.setHorizontalAlignment(SwingConstants.RIGHT);
 		textField_3.setText("5,99");
-		textField_3.setBounds(721, 412, 122, 30);
-		contentPane.add(textField_3);
 		textField_3.setColumns(10);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 95, 454, 422);
-		contentPane.add(scrollPane);
-		
+
 		table = new JTable();
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
@@ -174,33 +143,106 @@ public class TelaVenda extends JFrame {
 				{null, null, null, null, null},
 			},
 			new String[] {
-				"C\u00F3digo", "Descri\u00E7\u00E3o", "Valor", "Qnt", "Total"
+				"C\u00F3digo", "Descri\u00E7\u00E3o", "Valor", "Qn", "Total"
 			}
 		));
+		table.getColumnModel().getColumn(0).setPreferredWidth(100);
 		table.getColumnModel().getColumn(1).setPreferredWidth(359);
-		table.getColumnModel().getColumn(3).setPreferredWidth(40);
+		table.getColumnModel().getColumn(3).setPreferredWidth(45);
 		scrollPane.setViewportView(table);
-		
+
 		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "Op\u00E7\u00F5es", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBorder(new TitledBorder(null, "Op\u00E7\u00F5es",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panel.setForeground(new Color(255, 255, 255));
-		panel.setBackground(new Color(153, 204, 255));
-		panel.setBounds(480, 450, 363, 135);
-		contentPane.add(panel);
+		panel.setBackground(UIManager.getColor("CheckBox.light"));
 		panel.setLayout(null);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("F2 Inserir C\u00F3digo de Barras");
 		lblNewLabel_1.setForeground(new Color(0, 0, 0));
 		lblNewLabel_1.setFont(new Font("Verdana", Font.PLAIN, 12));
 		lblNewLabel_1.setBounds(10, 24, 191, 20);
 		panel.add(lblNewLabel_1);
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(5)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(lblTotal, GroupLayout.PREFERRED_SIZE, 161, GroupLayout.PREFERRED_SIZE)
+							.addGap(44)
+							.addComponent(textField, GroupLayout.PREFERRED_SIZE, 197, GroupLayout.PREFERRED_SIZE))
+						.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 554, Short.MAX_VALUE))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(lblFoto, GroupLayout.PREFERRED_SIZE, 363, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(lblQuantidade, GroupLayout.PREFERRED_SIZE, 233, GroupLayout.PREFERRED_SIZE)
+							.addGap(53)
+							.addComponent(lblPreoUnitrio, GroupLayout.PREFERRED_SIZE, 77, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, 207, GroupLayout.PREFERRED_SIZE)
+							.addGap(79)
+							.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, 77, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtpnFarinhaDeManioca, GroupLayout.PREFERRED_SIZE, 363, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(lblProduto, GroupLayout.PREFERRED_SIZE, 180, GroupLayout.PREFERRED_SIZE)
+							.addGap(61)
+							.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, 122, GroupLayout.PREFERRED_SIZE))
+						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 363, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap())
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(20)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(0, 0, Short.MAX_VALUE)
+							.addComponent(lblFoto, GroupLayout.PREFERRED_SIZE, 210, GroupLayout.PREFERRED_SIZE)
+							.addGap(11)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblQuantidade, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblPreoUnitrio, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE))
+							.addGap(5)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
+								.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE))
+							.addGap(11)
+							.addComponent(txtpnFarinhaDeManioca, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE)
+							.addGap(11)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblProduto, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGap(4)
+									.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)))
+							.addGap(8)
+							.addComponent(panel, GroupLayout.PREFERRED_SIZE, 135, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(69)
+							.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 422, Short.MAX_VALUE)
+							.addGap(11)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+								.addComponent(textField, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblTotal, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE))))
+					.addGap(8))
+		);
+		contentPane.setLayout(gl_contentPane);
 
 		try {
 			UIManager
-					.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+					.setLookAndFeel("de.javasoft.plaf.synthetica.SyntheticaBlueLightLookAndFeel");
 			SwingUtilities.updateComponentTreeUI(this);
-		} catch (Exception erro) {
-			JOptionPane.showMessageDialog(null, "Erro ao mudat look");
+		} catch (Exception e) {
+			try {
+
+				UIManager
+						.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+				SwingUtilities.updateComponentTreeUI(this);
+			} catch (Exception erro) {
+				JOptionPane.showMessageDialog(null, "Erro ao mudat look");
+			}
 		}
 	}
 }
