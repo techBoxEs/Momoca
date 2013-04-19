fc0e34996ff300f6e7fefdf670b782d9097fd341
Pedro Henrique <aluno@SALA201-23.FACAD.BR>
2013-04-19 09:12:46 -0300

    Novas telas foram add

---
+++ Momoca/src/TelaCadastroUsuario.java
@@ -0,0 +1,46 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+
+public class TelaCadastroUsuario extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaCadastroUsuario frame = new TelaCadastroUsuario();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaCadastroUsuario() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNome = new JLabel("Nome:");
+		lblNome.setBounds(10, 55, 46, 14);
+		contentPane.add(lblNome);
+	}
+
+}
--- Momoca/src/TelaLogin.java
+++ Momoca/src/TelaLogin.java
@@ -17,6 +17,7 @@ import javax.swing.JButton;
 import javax.swing.ImageIcon;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
 
 
 public class TelaLogin extends JFrame {
@@ -59,7 +60,8 @@ public class TelaLogin extends JFrame {
 		panel.setLayout(null);
 		
 		JLabel lblId = new JLabel("ID Usu\u00E1rio:");
-		lblId.setBounds(20, 130, 62, 14);
+		lblId.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblId.setBounds(20, 130, 85, 14);
 		panel.add(lblId);
 		
 		JLabel label = new JLabel("");
@@ -67,15 +69,16 @@ public class TelaLogin extends JFrame {
 		panel.add(label);
 		
 		JLabel lblSenha = new JLabel("Senha:");
-		lblSenha.setBounds(20, 170, 62, 14);
+		lblSenha.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblSenha.setBounds(20, 170, 85, 14);
 		panel.add(lblSenha);
 		
 		pfSenha = new JPasswordField();
-		pfSenha.setBounds(85, 163, 117, 28);
+		pfSenha.setBounds(115, 165, 159, 28);
 		panel.add(pfSenha);
 		
 		tfId = new JTextField();
-		tfId.setBounds(85, 123, 117, 28);
+		tfId.setBounds(115, 125, 159, 28);
 		panel.add(tfId);
 		tfId.setColumns(10);
 		
@@ -93,13 +96,26 @@ public class TelaLogin extends JFrame {
 		btnCancelar.setBounds(149, 206, 109, 23);
 		panel.add(btnCancelar);
 		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\aluno\\Desktop\\logoLogin.png"));
+		lblNewLabel.setBounds(0, 0, 284, 88);
+		panel.add(lblNewLabel);
 		
+		
+
 		try {
 			UIManager
-					.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+					.setLookAndFeel("de.javasoft.plaf.synthetica.SyntheticaBlueLightLookAndFeel");
 			SwingUtilities.updateComponentTreeUI(this);
-		} catch (Exception erro) {
-			JOptionPane.showMessageDialog(null, "Erro ao mudat look");
-		}
+		} catch (Exception e) {
+			try {
+
+				UIManager
+						.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+				SwingUtilities.updateComponentTreeUI(this);
+			} catch (Exception erro) {
+				JOptionPane.showMessageDialog(null, "Erro ao mudat look");
+			}
+	}
 	}
 }
